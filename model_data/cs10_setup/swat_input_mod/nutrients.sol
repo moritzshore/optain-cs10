381371a9985ce7fdea5e0eceb0808cf0b2bb4b3a
moritz shore <moritzshore@gmail.com>
2023-05-30 11:30:00 -0400

    update model data

--- model_data/cs10_setup/swat_input_mod/nutrients.sol
+++ model_data/cs10_setup/swat_input_mod/nutrients.sol
@@ -1,3 +1,3 @@
-nutrients.sol: written by SWAT+ editor v2.1.0 on 2023-05-30 09:55 for SWAT+ rev.60.5.4 and modified by the CS10 workflow on 2023-05-30 10:17:03.701799
+nutrients.sol: written by SWAT+ editor v2.1.0 on 2023-05-30 10:39 for SWAT+ rev.60.5.4 and modified by the CS10 workflow on 2023-05-30 11:27:09.7194
 name   exp_co   lab_p   nitrate   fr_hum_act   hum_c_n   hum_c_p   inorgp   watersol_p   h3a_p   mehlich_p   bray_strong_p   description
 soilnut1   0.00050   20   8.5   0.02000   10.00000   80.00000   35.1   0.4   0.25000   1.20000   0.85000   
