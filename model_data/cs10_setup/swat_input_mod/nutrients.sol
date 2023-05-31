634f5012f3eef26faf300c512ed8ce361c3110a5
moritz shore <moritzshore@gmail.com>
2023-05-31 08:13:53 -0400

    Stage 2 DoctR

--- model_data/cs10_setup/swat_input_mod/nutrients.sol
+++ model_data/cs10_setup/swat_input_mod/nutrients.sol
@@ -1,3 +1,3 @@
-nutrients.sol: written by SWAT+ editor v2.1.0 on 2023-05-30 10:39 for SWAT+ rev.60.5.4 and modified by the CS10 workflow on 2023-05-30 18:26:22.644361
+nutrients.sol: written by SWAT+ editor v2.1.0 on 2023-05-30 10:39 for SWAT+ rev.60.5.4 and modified by the CS10 workflow on 2023-05-31 01:33:46.583648
 name   exp_co   lab_p   nitrate   fr_hum_act   hum_c_n   hum_c_p   inorgp   watersol_p   h3a_p   mehlich_p   bray_strong_p   description
 soilnut1   0.00050   20   8.5   0.02000   10.00000   80.00000   35.1   0.4   0.25000   1.20000   0.85000   
